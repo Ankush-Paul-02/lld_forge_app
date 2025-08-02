fada85d03912058213d2abcf052ee838d1f4c36a
Ankush-Paul-02 <ankushpaulclg2002@gmail.com>
2025-08-02 19:19:36 +0530

    add themes

--- app/src/main/java/com/devmare/lld_forge_app/ui/theme/Theme.kt
+++ app/src/main/java/com/devmare/lld_forge_app/ui/theme/Theme.kt
@@ -1,43 +1,50 @@
 package com.devmare.lld_forge_app.ui.theme
 
 import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = gradient1,
+    secondary = gradient2,
+    tertiary = gradient3,
+    background = backgroundColor,
+    surface = backgroundColor,
+    onPrimary = whiteColor,
+    onSecondary = whiteColor,
+    onTertiary = whiteColor,
+    onBackground = whiteColor,
+    onSurface = whiteColor,
+    error = errorColor,
+    onError = whiteColor
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = gradient1,
+    secondary = gradient2,
+    tertiary = gradient3,
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.Black,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    error = errorColor,
+    onError = Color.White
 )
 
 @Composable
 fun Lld_forge_appTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    darkTheme: Boolean = true, // Your app is always dark by default
+    dynamicColor: Boolean = false, // Disable dynamic color for consistent branding
+    content: @Composable () -> Unit,
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
